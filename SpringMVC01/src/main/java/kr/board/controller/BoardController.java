035a37d891482c4258fa447017da7b693f16283a
sh <sanghoon273@gmail.com>
2023-01-24 18:49:34 +0900

    스프링MVC01 게시판 구현

--- SpringMVC01/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/board/controller/BoardController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.board.entity.Board;
 import kr.board.mapper.BoardMapper;
@@ -37,4 +39,29 @@ public class BoardController {
 		mapper.boardInsert(vo); //
 		return "redirect://boardList.do"; // 리다이렉트
 	}
+	   @GetMapping("/boardContent.do")
+	   public String boardContent(@RequestParam("idx") int idx, Model model) { //?idx=값
+	      Board vo=mapper.boardContent(idx);
+	      //조회수 증가
+	      mapper.boardCount(idx);
+	      model.addAttribute("vo", vo);
+	      return "boardContent"; //jsp
+	   }
+	   @GetMapping("/boardDelete.do/{idx}")
+	   public String boardDelete(@PathVariable("idx")int idx) { // ?idx=6
+		   mapper.boardDelete(idx);
+		   return "redirect:/boardList.do";
+	   }
+	   @GetMapping("/boardUpdateForm.do/{idx}")
+	   public String boardUpdateForm(@PathVariable("idx")int idx,Model model) {
+		   Board vo = mapper.boardContent(idx);
+		   model.addAttribute("vo",vo);
+		   return "boardUpdate"; //boardUpdate.jsp
+	   }
+
+	   @PostMapping("/boardUpdate.do")
+	   public String boardUpdate(Board vo) { // idx,title,content
+		   mapper.boardUpdate(vo); // 수정
+		   return "redirect:/boardList.do"; 
+	   }
 }
--- SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -3,6 +3,7 @@ package kr.board.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
 
 import kr.board.entity.Board;
 
@@ -10,6 +11,10 @@ import kr.board.entity.Board;
 public interface BoardMapper {
 	public List<Board>getLists(); //전체리스트
 	public void boardInsert(Board vo);
+	public Board boardContent(int idx);
+	public void boardDelete(int idx);
+	public void boardUpdate(Board vo);
 	
-	
+	@Update("update myboard set count=count+1 where idx=#{idx}")
+	public void boardCount(int idx);
 }
