6c0543f942822c03fb6450c3e79efb5deecdb354
sh <sanghoon273@gmail.com>
2023-01-27 14:20:12 +0900

    Spring MVC02 게시판 구현

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -1,42 +1,59 @@
 package kr.board.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.board.entity.Board;
 import kr.board.mapper.BoardMapper;
- 
+
 @Controller
 public class BoardController {
-	
-	@Autowired
-	BoardMapper BoardMapper;
-	
-	@RequestMapping("/")
-	public String main( ) {
-		return "main";
-	}
-	
-	//@ResponseBody -> jackson-databind(객체를 JSOM데이터형식으로 변환시켜줌)
-	@RequestMapping("/boardList.do")
-	public @ResponseBody List<Board> boardList() {
-		List<Board> list =BoardMapper.getLists();
-		return list; // JSON 데이터 형식으로 변환(API)해서 리턴(응답)하게끔 
-	}
-	
-	@RequestMapping("/boardInsert.do")
-	public @ResponseBody void boardInsert(Board vo) {
-		BoardMapper.boardInsert(vo);
-	}
-	
-}
+   
+//   @Autowired
+//   BoardMapper boardMapper;
+   
+   @RequestMapping("/")
+   public String main() {
+      return "main";
+   }
+   
+//   //@ResponseBody -> jackson-databind(객체를 -> JSON 데이터포맷으로 변환)
+//   @RequestMapping("/boardList.do")
+//   public @ResponseBody List<Board> boardList() {
+//      List<Board> list = boardMapper.getLists();
+//      return list; // JSON 데이터 형식으로 변환(API)해서 리턴(응답)하겠다.
+//   }
+//   
+//   @RequestMapping("/boardInsert.do")
+//   public @ResponseBody void boardInsert(Board vo) {
+//      boardMapper.boardInsert(vo);
+//   }
+//   
+//   @RequestMapping("/boardDelete.do")
+//   public @ResponseBody void boardDelete(@RequestParam("idx") int idx) {
+//      boardMapper.boardDelete(idx);
+//   }
+//   
+//   @RequestMapping("/boardUpdate.do")
+//   public @ResponseBody void boardUpdate(Board vo) {
+//      boardMapper.boardUpdate(vo);
+//   }
+//   
+//   @RequestMapping("/boardContent.do")
+//   public @ResponseBody Board boardContent(int idx) {
+//      Board vo = boardMapper.boardContent(idx);
+//      return vo;
+//   }
+//   
+//   @RequestMapping("/boardCount.do")
+//   public @ResponseBody Board boardCount(int idx) {
+//      boardMapper.boardCount(idx);
+//      Board vo = boardMapper.boardContent(idx);
+//      return vo;
+//   }
+ }
